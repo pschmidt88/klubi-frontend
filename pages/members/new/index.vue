4ddd3ac300498b2bdcd8d77ec1c5b8e1a59e132a
Paul Schmidt <rookian@gmail.com>
2019-05-05 12:46:13 +0200

    layout changes

--- pages/members/new/index.vue
+++ pages/members/new/index.vue
@@ -2,124 +2,124 @@
   <div class="animated fadeIn">
     <b-card header="Neues Mitglied hinzufügen">
       <b-container>
-      <b-row class="mt-5">
-        <b-col>
-          <b-tabs v-model="tabIndex" pills justified content-class="border-transparent mt-5">
-            <b-tab title="Persönliche Daten" active>
-              <b-container fluid>
-                <b-row>
-                  <b-col>
-                    <b-card-title>Persönliche Daten</b-card-title>
-    <b-card-sub-title>Name, Anschrift und Kontaktdaten des neuen Mitglieds</b-card-sub-title>
+        <b-row class="mt-5">
+          <b-col>
+            <b-tabs v-model="tabIndex" pills justified content-class="border-transparent mt-5">
+              <b-tab title="Persönliche Daten" active>
+                <b-container fluid>
+                  <b-row>
+                    <b-col>
+                      <b-card-title>Persönliche Daten</b-card-title>
+                      <b-card-sub-title>Name, Anschrift und Kontaktdaten des neuen Mitglieds</b-card-sub-title>
 
-    <b-card-text class="mt-5">
-      <b-row>
-        <b-col>
-          <b-form-group label="Vorname">
-            <b-form-input trim placeholder="Max"></b-form-input>
-          </b-form-group>
-        </b-col>
+                      <b-card-text class="mt-5">
+                        <b-row>
+                          <b-col>
+                            <b-form-group label="Vorname">
+                              <b-form-input trim placeholder="Max"></b-form-input>
+                            </b-form-group>
+                          </b-col>
 
-        <b-col>
-          <b-form-group label="Nachname">
-            <b-form-input trim placeholder="Mustermann"></b-form-input>
-          </b-form-group>
-        </b-col>
-      </b-row>
+                          <b-col>
+                            <b-form-group label="Nachname">
+                              <b-form-input trim placeholder="Mustermann"></b-form-input>
+                            </b-form-group>
+                          </b-col>
+                        </b-row>
 
-      <b-row>
-        <b-col>
-          <b-form-group label="Adresse">
-            <b-row>
-              <b-col cols="9">
-                <b-form-group label="Straße" label-size="sm">
-                  <b-form-input placeholder="Musterstraße"></b-form-input>
-                </b-form-group>
-              </b-col>
-              <b-col cols="3">
-                <b-form-group label="Hausnr." label-size="sm">
-                  <b-form-input placeholder="5"></b-form-input>
-                </b-form-group>
-              </b-col>
-            </b-row>
-          </b-form-group>
-        </b-col>
-      </b-row>
+                        <b-row>
+                          <b-col>
+                            <b-form-group label="Adresse">
+                              <b-row>
+                                <b-col cols="9">
+                                  <b-form-group label="Straße" label-size="sm">
+                                    <b-form-input placeholder="Musterstraße"></b-form-input>
+                                  </b-form-group>
+                                </b-col>
+                                <b-col cols="3">
+                                  <b-form-group label="Hausnr." label-size="sm">
+                                    <b-form-input placeholder="5"></b-form-input>
+                                  </b-form-group>
+                                </b-col>
+                              </b-row>
+                            </b-form-group>
+                          </b-col>
+                        </b-row>
 
-      <b-row>
-        <b-col sm="4">
-          <b-form-group label="Postleitzahl">
-            <b-form-input placeholder="12345"></b-form-input>
-          </b-form-group>
-        </b-col>
-        <b-col sm="8">
-          <b-form-group label="Ort">
-            <b-form-input placeholder="Musterstadt"></b-form-input>
-          </b-form-group>
-        </b-col>
-      </b-row>
+                        <b-row>
+                          <b-col sm="4">
+                            <b-form-group label="Postleitzahl">
+                              <b-form-input placeholder="12345"></b-form-input>
+                            </b-form-group>
+                          </b-col>
+                          <b-col sm="8">
+                            <b-form-group label="Ort">
+                              <b-form-input placeholder="Musterstadt"></b-form-input>
+                            </b-form-group>
+                          </b-col>
+                        </b-row>
 
-      <b-row>
-        <b-col cols="12" lg="6">
-          <b-form-group label="Geburtstag">
-            <b-row align-h="between">
-              <b-col>
-                <b-form-group label="Tag" label-size="sm" label-align="center">
-                  <b-form-input placeholder="16" class="text-center"></b-form-input>
-                </b-form-group>
-              </b-col>
+                        <b-row>
+                          <b-col cols="12" lg="6">
+                            <b-form-group label="Geburtstag">
+                              <b-row align-h="between">
+                                <b-col>
+                                  <b-form-group label="Tag" label-size="sm" label-align="center">
+                                    <b-form-input placeholder="16" class="text-center"></b-form-input>
+                                  </b-form-group>
+                                </b-col>
 
-              <b-col>
-                <b-form-group label="Monat" label-size="sm" label-align="center">
-                  <b-form-input placeholder="06" class="text-center"></b-form-input>
-                </b-form-group>
-              </b-col>
+                                <b-col>
+                                  <b-form-group label="Monat" label-size="sm" label-align="center">
+                                    <b-form-input placeholder="06" class="text-center"></b-form-input>
+                                  </b-form-group>
+                                </b-col>
 
-              <b-col>
-                <b-form-group label="Jahr" label-size="sm" label-align="center">
-                  <b-form-input placeholder="1988" class="text-center"></b-form-input>
-                </b-form-group>
-              </b-col>
-            </b-row>
-          </b-form-group>
-        </b-col>
-      </b-row>
-    </b-card-text>
-                  </b-col>
-                </b-row>
+                                <b-col>
+                                  <b-form-group label="Jahr" label-size="sm" label-align="center">
+                                    <b-form-input placeholder="1988" class="text-center"></b-form-input>
+                                  </b-form-group>
+                                </b-col>
+                              </b-row>
+                            </b-form-group>
+                          </b-col>
+                        </b-row>
+                      </b-card-text>
+                    </b-col>
+                  </b-row>
 
-                <b-row align-h="end">
-                  <b-button variant="primary" @click="nextTab">Weiter</b-button>
-                </b-row>
-              </b-container>
-            </b-tab>
+                  <b-row align-h="end">
+                    <b-button variant="primary" @click="nextTab">Weiter</b-button>
+                  </b-row>
+                </b-container>
+              </b-tab>
 
-            <b-tab title="Abteilung">
-              <b-container>
-                <b-row>
-                  <Department/>
-                </b-row>
-                <b-row align-h="between">
-                  <b-button variant="outline-primary" @click="previousTab">Zurück</b-button>
-                  <b-button variant="primary" @click="nextTab">Weiter</b-button>
-                </b-row>
-              </b-container>
-            </b-tab>
+              <b-tab title="Abteilung">
+                <b-container>
+                  <b-row>
+                    <Department/>
+                  </b-row>
+                  <b-row align-h="between">
+                    <b-button variant="outline-primary" @click="previousTab">Zurück</b-button>
+                    <b-button variant="primary" @click="nextTab">Weiter</b-button>
+                  </b-row>
+                </b-container>
+              </b-tab>
 
-            <b-tab title="Mitgliedsbeitrag">
-              <b-container>
-                <b-row>
-                  <MembershipFee/>
-                </b-row>
-                <b-row align-h="between">
-                  <b-button variant="outline-primary" @click="previousTab">zurück</b-button>
-                  <b-button variant="primary" @click="createMember">Weiter</b-button>
-                </b-row>
-              </b-container>
-            </b-tab>
-          </b-tabs>
-        </b-col>
-      </b-row>
+              <b-tab title="Mitgliedsbeitrag">
+                <b-container>
+                  <b-row>
+                    <MembershipFee/>
+                  </b-row>
+                  <b-row align-h="between">
+                    <b-button variant="outline-primary" @click="previousTab">zurück</b-button>
+                    <b-button variant="primary" @click="createMember">Weiter</b-button>
+                  </b-row>
+                </b-container>
+              </b-tab>
+            </b-tabs>
+          </b-col>
+        </b-row>
       </b-container>
     </b-card>
   </div>
