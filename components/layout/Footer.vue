ff50616823d81d7748eefaf3f1fc1dbcf14bb98a
Paul Schmidt <rookian@gmail.com>
2019-05-16 10:40:07 +0200

    wip: layout changes

---
+++ components/layout/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer class="main-footer">
+    <b-container>
+      <b-row>
+        <b-col xl="6" lg="6" md="6" cols="12" class="d-flex align-items-center justify-content-md-start justify-content-center">
+          <p class="text-gradient-02">Design By Saerox</p>
+        </b-col>
+        <div
+          class="col-xl-6 col-lg-6 col-md-6 col-sm-12 d-flex align-items-center justify-content-xl-end justify-content-lg-end justify-content-md-end justify-content-center"
+        >
+          <ul class="nav">
+            <li class="nav-item">
+              <a class="nav-link" href="documentation.html">Documentation</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="changelog.html">Changelog</a>
+            </li>
+          </ul>
+        </div>
+      </b-row>
+    </b-container>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ components/layout/Navigation.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="horizontal-menu">
+    <b-container>
+      <b-row>
+        <b-navbar type="light" toggleable="lg" class="main-menu">
+          <b-navbar-toggle target="main-navigation" data-toggle="collapse">
+            <span></span>
+            <span></span>
+            <span></span>
+          </b-navbar-toggle>
+
+          <b-collapse id="main-navigation" is-nav>
+            <b-navbar-nav class="mr-auto">
+              <b-nav-item>Dashboard</b-nav-item>
+              <b-nav-item-dropdown text="Mitgliederverwaltung">
+                <b-dropdown-item href="/members">Suche</b-dropdown-item>
+                <b-dropdown-item href="/members/details">Details</b-dropdown-item>
+                <b-dropdown-item href="/members/new">Mitglied anlegen</b-dropdown-item>
+              </b-nav-item-dropdown>
+            </b-navbar-nav>
+          </b-collapse>
+        </b-navbar>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
