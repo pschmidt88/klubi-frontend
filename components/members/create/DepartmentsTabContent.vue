f6599272c7eaf92ff395fa2b84650f0351eac91a
Paul Schmidt <rookian@gmail.com>
2019-05-24 16:04:10 +0200

    draft: adding members to departments

---
+++ components/members/create/DepartmentsTabContent.vue
@@ -0,0 +1,59 @@
+<template>
+  <b-container fluid>
+    <b-card
+      header="Zu Abteilung hinzufügen"
+      class="mb-5"
+      border-variant="light"
+    >
+      <department-row name="Fußball (Senioren)" action="remove" action-label="Entfernen">
+        <template v-slot:icon>
+          <icon-football/>
+        </template>
+      </department-row>
+    </b-card>
+
+    <b-card header="Verfügbare Abteilungen" class="mt-5" border-variant="light">
+      <department-row name="Gymnastik" action="add" action-label="Hinzufügen">
+        <template v-slot:icon>
+          <icon-gymnastics/>
+        </template>
+      </department-row>
+
+      <department-row name="Laufen">
+        <template v-slot:icon>
+          <icon-running/>
+        </template>
+      </department-row>
+
+      <department-row name="Taekwondo">
+        <template v-slot:icon>
+          <icon-taekwondo/>
+        </template>
+      </department-row>
+    </b-card>
+  </b-container>
+</template>
+
+<script>
+import DepartmentRow from "./DepartmentRow";
+import IconGymnastics from "~/components/icons/IconGymnastics";
+import IconFootball from "~/components/icons/IconFootball";
+import IconRunning from "~/components/icons/IconRunning";
+import IconTaekwondo from "~/components/icons/IconTaekwondo";
+
+export default {
+  name: "department-tab-content",
+  components: {
+    DepartmentRow,
+    IconGymnastics,
+    IconFootball,
+    IconRunning,
+    IconTaekwondo
+  },
+  data() {
+    return {
+      entry_date: "2019-05-24"
+    };
+  }
+};
+</script>
