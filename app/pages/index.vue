dde89f3416e022b70bb327b7270f301bbef18baa
p.schmidt <p.schmidt@shc-group.de>
2025-07-25 00:09:03 +0200

    Move backup to app

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,5 +6,7 @@
     <div class="aspect-video rounded-xl bg-muted/50" />
     <div class="aspect-video rounded-xl bg-muted/50" />
   </div>
-  <div class="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+  <div class="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
+    Insert 90s construction place gif here
+  </div>
 </template>
